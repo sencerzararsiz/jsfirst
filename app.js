52bdbc6f6ffcc45f2ffe21370315d2b1472686c5
sencerzararsiz <121825780+sencerzararsiz@users.noreply.github.com>
2023-02-14 01:14:19 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+// let sayi1 = 10;
+// let sayi2 = 20;
+// console.log(typeof false);
+
+
+// let ogrenciIsmi= "ahmet";
+// let ogrenciYasi= 24;
+
+// let sayi1= 10;
+// let sayi2 = 20;
+// let toplam = sayi1 + sayi2;
+// console.log(typeof toplam);
+
+// let toplam = Number(sayi1) + Number(sayi2);
+// console.log( typeof toplam);
+
+// console.log(typeof ogrenciIsmi);
+
+
